043e55d122ded33f23a61af3894049ec0133fc2e
Nikita Beloglazov <nikelandjelo@gmail.com>
2012-08-12 13:53:59 +0300

    Don't add start point.

--- misc/qtut/src/leiningen/new/qtut/project.clj
+++ misc/qtut/src/leiningen/new/qtut/project.clj
@@ -1,4 +1,3 @@
 (defproject {{name}} "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [quil/quil "1.6.0"]]
-  :main {{name}}.work)
+                 [quil/quil "1.6.0"]])
