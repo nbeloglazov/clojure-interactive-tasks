17769f68aec49f0e2dc68283b3df526bbb7b324c
Nikita Beloglazov <nikelandjelo@gmail.com>
2012-08-15 21:48:16 +0300

    Add task description.

--- 4_cellular_automaton/src/cellular_automaton/work.clj
+++ 4_cellular_automaton/src/cellular_automaton/work.clj
@@ -3,3 +3,27 @@
 
 
 
+;;; Your task is to implement cellular automaton.
+;;; The most famous example of cellular automaton is Conway's Game of Life.
+;;; Unlike previous tasks now you have to implement visualization and bots. So you need to implement everything :)
+;;; I suggest to use quil library for animation (it was used in all previous tasks): https://github.com/quil/quil
+;;; But of course you can use whatever you want.
+;;; Keep in mind that is should be simple to run your simulator with different automata (Game of Life is only 1 example).
+
+
+;;; Implement and run Brian's Brain automaton in your simulator: http://en.wikipedia.org/wiki/Brian%27s_Brain
+
+
+;;; Implement Wireworld automaton: http://en.wikipedia.org/wiki/Wireworld
+
+
+;;; Add Wireworld implementation to Rosetta Code (it's not present here yet): http://rosettacode.org/wiki/Wireworld
+
+
+;;; Implement Von Neumann cellular automaton: http://en.wikipedia.org/wiki/Von_Neumann_cellular_automata
+
+
+;;; Implement Langton's ant: http://en.wikipedia.org/wiki/Langton%27s_ant
+
+
+;;; Add ability to change cells' states by mouse click, to restart and pause simulation.
