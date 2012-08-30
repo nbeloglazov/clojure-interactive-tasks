2635b11503598c5afb76f0efc82cc76a5ddf1051
Nikita Beloglazov <nikelandjelo@gmail.com>
2012-08-31 01:27:19 +0300

    Add task description for k-means.

--- 5_k_means/src/k_means/work.clj
+++ 5_k_means/src/k_means/work.clj
@@ -1,8 +1,34 @@
 (ns k-means.work
-  (:use k-means.core))
+  (:use [k-means.core :only [run-empty run-2-circles run-3-circles run-random-circles]]))
 
-(defn solution []
-  1)
 
-(defn -main [& args]
-  (run solution))
+;;; Your task is to implement clustering algorithm.
+;;; You're a given a set of points on plane. And your goal is to divide them to k clusters.
+;;; Implement k-means algorithm to solve this task: http://en.wikipedia.org/wiki/K-means_clustering
+;;; Your function must take collection of points. Each point is a vector of x and y.
+;;; It must return collection of clusters. Each cluster - collection of points.
+;;; E.g. you have 4 points: [0 0] [1 1] [9 9] [10 10] and you need to partition them to 2 clusters.
+;;; Input will be [[0 0] [9 9] [1 1] [10 10]] and output should be something like [[[0 0] [1 1]] [[9 9] [10 10]]].
+;;; Note that you don't get k - number of clusters. You need to specify it somewhere in function.
+;;; To test you solution use following tests:
+
+; (run-empty SOLUTION)
+
+; (run-2-circles SOLUTION)
+
+; (run-3-circles SOLUTION)
+
+;;; Manipulation: mouse click - add new point
+;;;               space - reset simulation (remove all points or regenerate them, depenends on test)
+;;; Note that may need use different solutions (with k = 2 for run-2-circles and  k = 3 for run-3-circles).
+
+
+
+;;; Now try to improve your solution so it can determine k based on given points. So if there are visually 3 clusters it should partition points to 3 clusters, if 4 than to 4 clusters.
+;;; Test your solution on this test:
+
+; (run-random-circles SOLUTION)
+
+
+
+;;; Implement some other clustering algorithm.
