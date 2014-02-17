1420c1283923d8c1b0579e5720905deccd9570c7
Nikita Beloglazov <nikelandjelo@gmail.com>
2014-02-17 23:13:38 +0000

    Update task 3. Durak to latest clojure and quil.

--- 3_durak/src/durak/work.clj
+++ 3_durak/src/durak/work.clj
@@ -1,17 +1,17 @@
 (ns durak.work
-  (:use [durak.core :only (run-game)] ))
+  (:require [durak.core :refer [run-game]]))
 
 
-;;; Your task is to write bot for playing card game "Durak": http://en.wikipedia.org/wiki/Durak
-;;; Bot plays against another built-in bot (2 players).
+;;; Your task is to write a bot for playing card game "Durak": http://en.wikipedia.org/wiki/Durak
+;;; Your bot plays against another built-in bot (2 players).
 ;;; Bot is a map that contains 2 functions: function to attack and function to defend.
-;;; It must have following format: {:attack YOUR_ATTACK_FN  :defend YOUR_DEFEND_FN}
+;;; It must have following format: {:attack YOUR_ATTACK_FN :defend YOUR_DEFEND_FN}
 ;;; Attack and defend functions are similar: they both take same arguments and both must return a card.
 ;;; Attack function is called when your are an attacker and it should select 1 card from your hand and put it on the table.
-;;; Note that if it is not the first card in current attack, it must have the same rank as one of the cards on the table.
+;;; Note that if it is not the first card in current attack, it must have the same rank as one of the cards on the table (check rules).
 ;;; If you return nil it means that you don't want to attack and attack is finished.
 ;;; Defend function is called  when your are a defender and it should select 1 card from your hand and put it on the table.
-;;; If you return nil it means you can't (or don't want to) defend and you take all cards from the table.
+;;; If you return nil it means you can't (or don't want to) defend and you take all cards from the table to you hand.
 
 ;;; Card is a map with 2 keys: rank and suit.
 ;;; Rank is a number from 6 to 14.
@@ -19,7 +19,7 @@
 ;;; 12 - Queen
 ;;; 13 - King
 ;;; 14 - Ace
-;;; Suit is one of the 4 keywords: :spades :hearts :diamonds :clubs
+;;; Suit is one of the 4 keywords: :spades, :hearts, :diamonds or :clubs
 ;;; Examples: {:rank 6, :suit :clubs}  {:rank 14, :suit :hearts}
 
 ;;; Functions input:
@@ -30,33 +30,33 @@
 ;;;  :trump :clubs}
 ;;; Hand is a sequence of cards in your hand.
 ;;; Table is a sequence of cards already on table.
-;;; If you are an attacker and it's start of an attack then table will be empty.
-;;; If your are a defender then table will be non-empty and your need to beat last card from the table.
-;;; Cards on the table can be represented like: attack  - defense - attack - defense - attack. Odd cards are attack, even cards are defense.
+;;; If you are an attacker and it's the start of the attack then table will be empty.
+;;; If you are a defender then table will be non-empty and your need to beat last card from the table.
+;;; Cards on the table can be represented like: attack  - defense - attack - defense - attack. Odd cards are attack cards, even cards are defense cards.
 ;;; Trump is trump suit of the game.
 
-;;; To test your solution call (run-game YOUR_SOLUTION)
-;;; Your bot is the player in the lower part of screen (cards a visible to your).
+;;; To test your solution use run-game function: (run-game YOUR_SOLUTION)
+;;; Your bot is the player in the bottom part of screen (cards a visible to your).
 ;;; To run next action press SPACE, to restart game press R.
 ;;; When game is over (one of the players has no cards in his hand) nothing will happen when your press SPACE.
 ;;; If your press SPACE and nothing happen and game is not over yet, look at stacktraces, probably your bot (or built-in bot) tries to perform invalid atttack or defense.
 
 
 
-;;; Implement program that takes 2 bots, runs game and return winner.
+;;; Implement program that takes 2 bots, runs game and returns winner.
 ;;; Study init-game and next-action function in the src/durak/logic.clj. They can be used to run game.
 
 
 
 ;;; Implement bot that memorizes all cards played in game and use some smarter logic based on probability of opponents cards.
-;;; Use clojure's atoms or refs to keep data between moves.
+;;; Use clojure atoms or refs to store data between moves.
 ;;; If you've implemented this bot in the first task then you can skip it :)
 
 
 
-;;; Implement attack and defense functions that they ask user input. So human can play.
+;;; Implement attack and defense functions that they ask user input so human can play.
 ;;; I don't know how to do it, may be some hacks with swing like creating and invoking dialog to ask for user input.
 
 
 
-;;; Modify program such that it can play with 3 or 4 players.
+;;; Modify program so it 3 or 4 players can play at the same table.
