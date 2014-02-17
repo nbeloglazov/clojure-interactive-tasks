2629444e3f6c26f178a306e385318d777b35bd54
Nikita Beloglazov <nikelandjelo@gmail.com>
2014-02-17 22:57:42 +0000

    Update task 2. Snake to latest clojure and quil.

--- 2_snake/src/snake/work.clj
+++ 2_snake/src/snake/work.clj
@@ -1,14 +1,14 @@
 (ns snake.work
-  (:use [snake.core :only (run-not-grow run-grow run-many-apples run-with-walls)]))
+  (:require [snake.core :refer (run-not-grow run-grow run-many-apples run-with-walls)]))
 
 ;;; You're writing a bot for playing snake.
 ;;; So, you are a snake and your goal is to collect apples.
-;;; Field sizes: 40 x 30
-;;; Every turn you move to one of the neighbours cell.
-;;; Your function must take 2 arguments: snake's position and apple's position and decide which direction to move.
+;;; Field size: 40 x 30
+;;; Every turn you move to one of the adjacent cell.
+;;; Your function must take 2 arguments: snake position and apple position and decide which direction to move.
 ;;; Directions are: :up, :down, :left, :right (they are keywords). Your function must return one of these directions.
-;;; Position (snake's or apple's) is a vector of 2 elements: x and y.
-;;; In this task snake is not growing after it ate an apple so there is no danger of snake hitting itself.
+;;; Position (snake or apple) is a vector of 2 elements: x and y.
+;;; In this task snake is not growing from eating apples so there is no danger of snake hitting itself.
 ;;; Note: upper left corner cell is (0, 0).
 
 ;;; Uncomment and substitute your solution
@@ -16,24 +16,23 @@
 
 
 
-;;; Snake grows now (each time snake eats an apple, it's length increases).
+;;; Snake is growing now. (each time snake eats an apple, body length increases).
 ;;; You need to write similar function as in previous task.
 ;;; It takes 2 arguments.
-;;; First argument is snake's body - collection of cells, each cell is a vector of x and y. First cell is snake's head.
-;;; Second argument is apple's position - vector of x and y.
+;;; First argument is snake body - collection of cells, each cell is a vector of x and y. First cell is the head.
+;;; Second argument is apple position - vector of x and y.
 ;;; It should return direction: :up, :down, :left or :right.
 ;;; Note that you cannot change direction to the opposite in 1 move: snake will hit it's tail if length is 2 or more.
-;;; Wait, you can change direction but snake will die :\
+;;; Well, you can change direction but snake will die :\
 
 ;;; Uncomment and substitute your solution
 ; (run-grow YOUR_SOLUTION_HERE)
 
 
 
-;;; Now you have many apples (5) instead of one.
+;;; Now you have many apples (five) instead of one.
 ;;; Function the same as previous but it takes set of apples instead of the single apple.
 ;;; Each apple in the set is a vector of x and y.
-;;; E.g. you can try to reach nearest apple to the snake.
 
 ;;; Uncomment and substitute your solution
 ; (run-many-apples YOUR_SOLUTION_HERE)
@@ -42,7 +41,7 @@
 
 ;;; Walls are added. So snake can hit wall and die.
 ;;; Your function now takes third argument - set of walls.
-;;; Each wall is a cell that snake is not allowed to  move to.
+;;; Each wall is a cell that snake is not allowed to bump to.
 ;;; Wall is a vector of x and y.
 
 ;;; Uncomment and substitute your solution
